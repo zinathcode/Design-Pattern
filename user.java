f6f7737840eff43ae2aeb02fa599c08d78e6ef10
zinathcode <139478886+zinathcode@users.noreply.github.com>
2023-07-13 21:49:03 +0600

    Add files via upload

---
+++ user.java
@@ -0,0 +1,17 @@
+public class user {
+    
+    public void login(String username, String password) {
+        //System.out.printf("Login Service");
+    }
+
+    public static void main(String[] args) {
+        EmailService emailService = new EmailService();
+        emailService.sendEmail("TEST", "TEST", "TEST"); 
+    }
+}
+
+class EmailService {
+    public void sendEmail(String to, String subject, String body) {
+        System.out.printf("Email Service");
+    }
+}
